ba9802ea43b56c7ca6ffbc64cece5a46eb27d60b
Carlos Eduardo Delgado Rivera <cedr1990@gmail.com>
2024-07-14 10:31:46 -0600

    se hizo useEffect y componente ImageCard se agrego

--- 07-giphy-app/src/App.jsx
+++ 07-giphy-app/src/App.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  
+   // se crea el estado que se va a encargar de actualizar la lista de gifs
+const [gifs, setGifs] = useState([]);
+const apiKey = import.meta.env.VITE_GIPHY_API_KEY
+
+// las llamadas a las API generalmente se hacen en useEffect
+// Usarermos useEffect con un arreglo de dependencias vacío para que se ejecute una sola vez
+useEffect(() => {
+  fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}vfaP4Tc1fHUGRAJBnmz7EW3G7yGU4Ngl&limit=25&offset=0&rating=g&bundle=messaging_non_clips`)
+    .then((response) => response.json())
+    .then(result =>{
+      console.log(result);
+      setGifs(result.data)
+    }).catch((error) => console.log(error))
+}, []);
   return (
     <>
      <div>
---
+++ 07-giphy-app/src/components/ImageCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageCard = ({tittle,url}) => {
+  return (
+    <div className='image-container'>
+        <img className='image-view' src={url} alt={tittle} />
+    </div>
+  )
+}
+
+export default ImageCard
