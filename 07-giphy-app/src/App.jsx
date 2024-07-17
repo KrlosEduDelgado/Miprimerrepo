44554551415a4c60747878627ef3b0b6d90d369f
Carlos Eduardo Delgado Rivera <cedr1990@gmail.com>
2024-07-16 20:03:50 -0600

    Se carga la actividad ya con los documentos guardados y ya se puede ver

--- 07-giphy-app/src/App.jsx
+++ 07-giphy-app/src/App.jsx
@@ -1,26 +1,39 @@
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from "react"
 import './App.css'
+import ImageCard from "./components/ImageCard"
 
 function App() {
-   // se crea el estado que se va a encargar de actualizar la lista de gifs
-const [gifs, setGifs] = useState([]);
-const apiKey = import.meta.env.VITE_GIPHY_API_KEY
-
-// las llamadas a las API generalmente se hacen en useEffect
-// Usarermos useEffect con un arreglo de dependencias vacío para que se ejecute una sola vez
-useEffect(() => {
-  fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}vfaP4Tc1fHUGRAJBnmz7EW3G7yGU4Ngl&limit=25&offset=0&rating=g&bundle=messaging_non_clips`)
-    .then((response) => response.json())
+  //Creo el estado que se va a encargar de actualizar la lista de gifs
+  const [gifs, setGifs] = useState([])
+  const apiKey = import.meta.env.VITE_GIPHY_API_KEY
+  //Las llamadas a las API generalmente se hacen en el useEffect
+  //Usaremos useEffect con un arreglo de dependencias vacío para que se ejecute una sola vez
+  useEffect(() =>{
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`)
+    .then(response => response.json())
     .then(result =>{
       console.log(result);
       setGifs(result.data)
-    }).catch((error) => console.log(error))
-}, []);
+    }).catch(error => console.log(error))
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <>
-     <div>
-      <h1>Giphy App</h1>
-     </div>
+      <div>
+        <h1>Giphy App</h1>
+        <div className="grid-cards">
+          {
+            gifs.map((gif, index) =>(
+              <ImageCard 
+                key={gif.id + index}
+                url={gif.images.fixed_height.url}
+                title={gif.title}
+              />
+            )
+          )}
+        </div>
+      </div>
     </>
   )
 }
