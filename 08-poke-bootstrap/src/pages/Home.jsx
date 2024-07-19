8ef6c319f92976094dba5c12216b30e97c061db5
Carlos Eduardo Delgado Rivera <cedr1990@gmail.com>
2024-07-19 16:45:06 -0600

    pokemon details con params

--- 08-poke-bootstrap/src/pages/Home.jsx
+++ 08-poke-bootstrap/src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    const [pokemons, setPokemons] = useState([]) //lista de pokemones
-    const [searchTerm, setSearchTerm] = useState("") //palabra de busqueda
+    const [pokemons, setPokemons] = useState([]) //Lista de Pokemones
+    const [searchTerm, setSearchTerm] = useState("")//Palabra que queremos buscar
+    
     useEffect((() => {
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
         .then(res => res.json())
         .then(data => setPokemons(data.results))
         .catch(error => console.error(error))
@@ -14,44 +16,46 @@ const Home = () => {
         setSearchTerm(event.target.value)
     }
 
-    const filteredPokemons = pokemons.filter((pokemon) => {
+    const filteredPokemons = pokemons.filter(pokemon => {
         return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
-    })
+    }
+
+    )
 
     return (
-        
-
-    <div className='container'>
+    <div className="container">
         <h1>Home</h1>
-        <form className="form inline my-2 w-100">
+        <form className="form-inline my-2 w-100">
             <input 
-            type="text"
-            id="search"
-            className="form-control"
-            placeholder='ingrese el pokemon a buscar'
-            onChange={handleSearch}
-            value={searchTerm}
-            />
+                type="text" 
+                id="search"
+                className="form-control"
+                placeholder="Ingrese el pokemon que desea buscar"
+                onChange={handleSearch}
+                value={searchTerm}
+             />
         </form>
-        <div className='row'>
+        <div className="row">
             {
-                //despliego mi lista de pokemones
-                filteredPokemons.map(pokemon => (
-                    <div className='col-sm-4 mb-4' key={pokemon.name}>
-                        <div className='card'> 
-                            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split("/")[6]}.png`} alt="{pokemon.name}" />
-                            <div className='card-body'>
-                                <h5 className='card-title'>{pokemon.name}</h5>
+                //Despliego mi lista de pokemones
+                filteredPokemons.map(pokemon =>(
+                    <div className="col-sm-4 mb-4" key={pokemon.name}>
+                        <div className="card">
+                            <img 
+                                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split("/")[6]}.png`} 
+                                alt={pokemon.name} 
+                            />
+                            <div className="card-body">
+                                <Link to={`/pokemon/${pokemon.url.split("/")[6]}`}>
+                                    <h5 className="card-title">{pokemon.name}</h5>
+                                </Link>
                             </div>
-
                         </div>
                     </div>
                 ))
             }
-
         </div>
     </div>
   )
 }
-
 export default Home
