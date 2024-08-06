0ed1ac230e8ad5e8a79f2c909b94fae31c7453da
Carlos Eduardo Delgado Rivera <cedr1990@gmail.com>
2024-08-05 20:06:02 -0600

    se agregan ultimas modificaciones al proyecto

--- 5-PROYECTO-FINAL/src/App.jsx
+++ 5-PROYECTO-FINAL/src/App.jsx
@@ -1,34 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import YouTube from 'react-youtube'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_URL = 'https://api.themoviedb.org/3'
+  const API_KEY = 'cb3849bed6d2a14da0ef939ce4488152'
+  const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
+  const URL_IMAGE  = 'https://image.tmdb.org/t/p/original'
+
+// Variables de estado
+const [movies, setMovies] = useState([]);
+const [searchKey, setSearchKey] = useState('');
+const [trailerUrl, setTrailerUrl] = useState(null);
+const [movie, setMovie] = useState({title:"Loading Movies"});
+const [playing, setPlaying] = useState(false);
+
+// Define the fetchMovies function
+const fetchMovies = async () => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchKey}`
+    );
+    setMovies(response.data.results);
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+  }
+};
+
+useEffect(() => {
+  fetchMovies();
+}, [searchKey]);
+
+
+// funcion para realizar la peticion por get a la API 
+
+const searchMovies = async (searchKey) => {
+  const type = searchKey ? 'search' : 'discover';
+
+  const {data: {results}} = await axios.get(`${API_URL}/${type}/movie`, {
+    params: {
+      api_key: API_KEY,
+      query: searchKey
+    }
+  });
+  setMovies(results);
+  setMovie(results[0]);
+} 
+
+useEffect(() => {
+  fetchMovies();
+}, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      {/* contenedor que va a mostrar  los posters de las peliculas actuales */}
+      <div className="container mt-3">
+        <div className="row">
+          {movies.map ((movie) => (
+            <div key={movie.id} className="col-md-4 mb-3">
+               <img src= {`${URL_IMAGE + movie.poster_path}`} alt="" height={600} width= "100" />
+               <h4 className="text-center">{movie.title}</h4>
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
