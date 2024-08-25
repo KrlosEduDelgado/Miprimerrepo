776c224461aad8493ab9873a4fc79f75a74156f1
Carlos Eduardo Delgado Rivera <cedr1990@gmail.com>
2024-08-25 14:16:02 -0600

    Se cargo la clase del 20

--- 6.2.1-react-auth/src/components/Header/Header.jsx
+++ 6.2.1-react-auth/src/components/Header/Header.jsx
@@ -1,49 +1,72 @@
 import { NavLink } from 'react-router-dom'
+import { useAuthContext } from '../../hooks/useAuth'
 import './header.scss'
 
 const Header = () => {
-const linkIsActive = (isActive) =>  isActive? 
-'header__item-link--is-active header__item-link' : 'header__item-link'   
+  const { logout, isAuth } = useAuthContext()
 
+  const linkIsActive = (isActive) => isActive ? 'header__item-link header__item-link--is-active' : 'header__item-link'
 
   return (
     <nav className='header'>
         <NavLink to='/' className='header__logo'>LOGO</NavLink>
         <ul className='header__nav-list'>
             <li className='header__list-item'>
-                <NavLink 
-                    to="/" 
-                    className={({isActive})=> linkIsActive(isActive) }
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => linkIsActive(isActive)}
                 >Home
                 </NavLink>
             </li>
+
             <li className='header__list-item'>
-                <NavLink 
-                    to="/dashboard" 
-                    className={({isActive})=> linkIsActive(isActive) }
-                >Dashboard
+                <NavLink
+                  to="/dashboard"
+                  className={({ isActive }) => linkIsActive(isActive)}>Dashboard
                 </NavLink>
             </li>
-            <li className='header__list-item'>
-                <NavLink 
-                    to="/secret" 
-                    className={({isActive})=> linkIsActive(isActive) }
-                >Secret </NavLink>
-            </li>
-            <li className='header__list-item'>
-                <NavLink 
-                    to="/login" 
-                    className={({isActive})=> linkIsActive(isActive) }
-                >Login
-                </NavLink>
-            </li>
-            <li className='header__list-item'>
-                <NavLink 
-                to="/signup" 
-                className={({isActive})=> linkIsActive(isActive) }
-                >Signup
-                </NavLink>
-            </li>
+
+            {isAuth
+              ? (
+                <>
+                  <li className='header__list-item'>
+                      <NavLink
+                        to="/secret"
+                        className={({ isActive }) => linkIsActive(isActive)}
+                      >Secret
+                      </NavLink>
+                  </li>
+
+                  <li className='header__list-item'>
+                      <NavLink
+                        to="/"
+                        className='header__item-link'
+                        onClick={logout}
+                      >Logout
+                      </NavLink>
+                  </li>
+                </>
+              )
+              : (
+                <>
+                  <li className='header__list-item'>
+                      <NavLink
+                        to="/login"
+                        className={({ isActive }) => linkIsActive(isActive)}
+                      >Login
+                      </NavLink>
+                  </li>
+
+                  <li className='header__list-item'>
+                      <NavLink
+                        to="/signup"
+                        className={({ isActive }) => linkIsActive(isActive)}
+                      >Signup
+                      </NavLink>
+                  </li>
+                </>
+              )
+            }
         </ul>
     </nav>
   )
--- 6.2.1-react-auth/src/pages/Login.jsx
+++ 6.2.1-react-auth/src/pages/Login.jsx
@@ -1,44 +1,77 @@
+import { useForm } from 'react-hook-form'
+import { loginUserService } from '../services/userServices'
+import { useAuthContext } from '../hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
 import logo from '../assets/react.svg'
-import  '../styles/form.css'
+import '../styles/form.css'
 
 const Login = () => {
+  const navigate = useNavigate()
+
+  const { login } = useAuthContext()
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await loginUserService(data)
+      if(response.status === 200) {
+        // Guardamos el token en el localStorage del navegador.
+        // Este dato permanece aún si el navegador se cierra y se vuelve a abrir.
+        login(response.data.token)
+        navigate('/dashboard')
+      }
+    } catch (error) {
+      console.error('Ocurrio un error al iniciar sesión', error.message)
+    }
+  }
+
   return (
     <main className="form-signin w-100 m-auto">
-  <form>
-    <img
-      className="mb-4"
-      src={logo}
-      alt=""
-      width={72}
-      height={57}
-    />
-    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-    <div className="form-floating">
-      <input
-        type="email"
-        className="form-control"
-        id="floatingInput"
-        placeholder="name@example.com"
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <img
+        className="mb-4"
+        src={logo}
+        alt=""
+        width={72}
+        height={57}
       />
-      <label htmlFor="floatingInput">Email address</label>
-    </div>
-    <div className="form-floating">
-      <input
-        type="password"
-        className="form-control"
-        id="floatingPassword"
-        placeholder="Password"
-      />
-      <label htmlFor="floatingPassword">Password</label>
-    </div>
-    
-    <button className="btn btn-primary w-100 py-2" type="submit">
-      Sign in
-    </button>
-    <p className="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
-  </form>
-</main>
+      <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+      <div className="form-floating">
+        <input
+          type="email"
+          className="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          {...register('email', { required: true })}
+        />
+        <label htmlFor="floatingInput">Email address</label>
+      </div>
+      {errors.email && <span>This field is required</span>}
+
+      <div className="form-floating">
+        <input
+          type="password"
+          className="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+          {...register('password', { required: true })}
+        />
+        <label htmlFor="floatingPassword">Password</label>
+      </div>
+      {errors.password && <span>This field is required</span>}
+
+      <button className="btn btn-primary w-100 py-2" type="submit">
+        Sign in
+      </button>
+      <p className="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
+    </form>
+  </main>
   )
 }
-
 export default Login
