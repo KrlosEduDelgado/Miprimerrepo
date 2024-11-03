fd7f298adfcd1bc38e7f511da4ad63d31bc841d2
Carlos Eduardo Delgado Rivera <cedr1990@gmail.com>
2024-11-03 14:30:30 -0600

    Nuevo proyecto

--- 8.2.1_cars-api/index.js
+++ 8.2.1_cars-api/index.js
@@ -1,6 +1,7 @@
 // estructura básica de un servidor
 import express from 'express'
 import { connect } from './config/database.js'
+import carRoutes from './routes/carRoutes.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -9,6 +10,7 @@ const api = express()
 api.use(express.json()) // parsear el body de las peticiones a JSON
 
 // Rutas
+api.use('/api/v1', carRoutes)
 
 // nos conectamos a la base de datos y luego levantamos el servidor
 connect().then(() => {
---
+++ 8.2.1_cars-api/routes/carRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+// import { createCar } from '../controllers/carController.js'
+import * as carController from '../controllers/carController.js' // Importar todo el controlador con el operador *
+
+const carRoutes = express.Router()
+
+carRoutes.post('/cars', carController.createCar)
+carRoutes.get('/cars', carController.getAllCars)
+carRoutes.get('/cars/:carId', carController.getCarById)
+carRoutes.patch('/cars/:carId', carController.updateCarById)
+carRoutes.delete('/cars/:carId', carController.deleteCarById)
+
+export default carRoutes
