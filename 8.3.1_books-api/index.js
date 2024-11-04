b30e91086a29b0218b8e63bb80431d8bdb900a44
Carlos Eduardo Delgado Rivera <cedr1990@gmail.com>
2024-11-03 20:45:45 -0600

    Se carga la clase del 24 de oct

---
+++ 8.3.1_books-api/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { connect } from "./config/database.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+app.use(express.json())
+
+//aqui van las rutas
+app.use('/api/v1/books', bookRoutes)
+// nos conectamos a la base de datos y luego levantamos el servidor
+connect().then(() => {
+    app.listen(PORT, () => {
+      console.log(`API running on http://localhost:${PORT} 🚀`)
+    })
+  })
