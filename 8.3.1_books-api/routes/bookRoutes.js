8175e01426c5a2652f4dd93e8593e43b4b8f6315
Carlos Eduardo Delgado Rivera <cedr1990@gmail.com>
2024-11-04 23:00:39 -0600

    Se crearon Routes de get, patch y se comprobo en insomnia

--- 8.3.1_books-api/routes/bookRoutes.js
+++ 8.3.1_books-api/routes/bookRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import { createBook } from '../controllers/bookController.js'
+import { createBook, getAllBooks, getBookById, updateBookById } from '../controllers/bookController.js'
 
 const bookRoutes = express.Router()
 
 // Rutas de libros
 bookRoutes.post('/', createBook)
+bookRoutes.get('/', getAllBooks)
+bookRoutes.get('/:bookId', getBookById) // super importante poner los dos puntos despues de la barra
+bookRoutes.patch('/:bookId', updateBookById)
 
 export default bookRoutes
