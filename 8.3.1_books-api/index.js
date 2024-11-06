0e8af7239638a148fcd521b7fb370245a4422f7f
Carlos Eduardo Delgado Rivera <cedr1990@gmail.com>
2024-11-05 22:02:42 -0600

    Se agrego el morgan

--- 8.3.1_books-api/index.js
+++ 8.3.1_books-api/index.js
@@ -1,17 +1,19 @@
-import express from "express";
-import { connect } from "./config/database.js";
-import bookRoutes from "./routes/bookRoutes.js";
+import express from 'express'
+import { connect } from './config/database.js'
+import bookRoutes from './routes/bookRoutes.js'
+import morgan from 'morgan'
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
-//aqui van las rutas
+// aqui van las rutas
 app.use('/api/v1/books', bookRoutes)
 // nos conectamos a la base de datos y luego levantamos el servidor
 connect().then(() => {
-    app.listen(PORT, () => {
-      console.log(`API running on http://localhost:${PORT} 🚀`)
-    })
+  app.listen(PORT, () => {
+    console.log(`API running on http://localhost:${PORT} 🚀`)
   })
+})
